206ddd77ec87e94d7b60f3b0d3177c85a1517819
Matheus Alencar <matheusalencar6942@gmail.com>
2023-11-27 11:54:23 -0300

    customizando tema do angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AngularMaterialModule } from './angular-material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AngularMaterialModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-material-custom-theme';
